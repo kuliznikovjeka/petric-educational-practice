3ede6c0483c525bd6e9239ee529f3435b612a71a
Jeka <example@gmail.com>
2023-07-19 23:03:25 +0300

    Первая практика, тупил но думал

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 "use strict";
 
-let lol = 0;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms);
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const questionOne = prompt('Один из последних просмотренных фильмов?', '');
+const questionTwo = +prompt('На сколько оцените его?', '');
+const questionThree = prompt('Один из последних просмотренных фильмов?', '');
+const questionFour = +prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionThree] = questionFour;
+console.log(personalMovieDB);
